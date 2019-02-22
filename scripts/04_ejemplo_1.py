544b30e574ea2c809568163abdfac277aff6b306
Juan Fiol <juanfiol@gmail.com>
2019-02-22 13:19:00 -0300

    Ejercicios de clase 4

---
+++ scripts/04_ejemplo_1.py
@@ -0,0 +1,4 @@
+conj = set("Hola amigos, como están")
+for elem in conj:
+  print (elem, end='')
+print('\n')
--- scripts/ejemplo_07_1.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-x, y = np.loadtxt('../data/ejemplo_plot_07_1.dat', unpack=True)
-plt.plot(x, y)
